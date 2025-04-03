c67d4a6e452fce63b49d6383e000316bd3d10997
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-04-03 15:27:26 +0530

    chore: setup ssr template

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,17 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -24,5 +28,5 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
-  },
-)
+  }
+);
---
+++ src/entry-client.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import { hydrateRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+hydrateRoot(
+  document.getElementById('root'),
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
