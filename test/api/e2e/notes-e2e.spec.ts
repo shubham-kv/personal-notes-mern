fb8cb69a430706c548322662cd35384daf24030a
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-04-06 16:47:16 +0530

    test(api): add tests for get note API

--- test/api/e2e/notes-e2e.spec.ts
+++ test/api/e2e/notes-e2e.spec.ts
@@ -3,23 +3,35 @@ import supertest, { Response } from 'supertest';
 import TestAgent from 'supertest/lib/agent';
 
 import { createApp } from '@server/app';
+import { Note } from '@server/models';
 import { CreateNoteData, GetNotesQueryParams } from '@shared/types/api';
 
 import { runDBHooks } from '../setup';
 import { createNoteDataStub } from '../stubs';
-import { invalidCreateNoteData, invalidGetNotesQueryParams } from '../data';
-import { notesResourcePath } from '../constants';
+import {
+  invalidCreateNoteData,
+  invalidGetNotesQueryParams,
+  invalidIdParams,
+} from '../data';
+import { noteResourcePath, notesResourcePath } from '../constants';
 
 describe('Notes API e2e', () => {
   const createNoteRequestLine = `POST ${notesResourcePath}`;
   const getNotesRequestLine = `GET ${notesResourcePath}`;
+  const getNoteRequestLine = `GET ${noteResourcePath}`;
+
   let testAgent: TestAgent;
+  const seededNoteIds: string[] = [];
+
+  runDBHooks(true);
 
   beforeAll(async () => {
     const app = await createApp();
     testAgent = supertest(app);
+
+    const seededNotes = await Note.find();
+    seededNoteIds.push(...seededNotes.map((n) => n.id));
   });
-  runDBHooks(true);
 
   describe(`Create Note API '${createNoteRequestLine}'`, () => {
     describe.each(invalidCreateNoteData)(
@@ -39,7 +51,7 @@ describe('Notes API e2e', () => {
           expect(response.body).toMatchObject({
             error: expect.stringMatching(/bad/gi),
             message: expect.stringMatching(/fail/gi),
-          })
+          });
         });
       }
     );
@@ -83,7 +95,7 @@ describe('Notes API e2e', () => {
           expect(response.body).toMatchObject({
             error: expect.stringMatching(/bad/gi),
             message: expect.stringMatching(/fail/gi),
-          })
+          });
         });
       }
     );
@@ -115,6 +127,77 @@ describe('Notes API e2e', () => {
           total: expect.any(Number),
           pageLimit: query.pageLimit,
           page: query.page,
+        });
+      });
+    });
+  });
+
+  describe(`Get Note API '${getNoteRequestLine}'`, () => {
+    describe.each(invalidIdParams)(
+      'when requested with invalid id parameter',
+      (idParam) => {
+        let response: Response;
+
+        beforeEach(async () => {
+          response = await testAgent.get(
+            noteResourcePath.replace(':id', idParam)
+          );
+        });
+
+        test(`should fail with '400 Bad Request'`, ({ expect }) => {
+          expect(response.statusCode).toBe(400);
+        });
+
+        test(`should respond with error`, ({ expect }) => {
+          expect(response.body).toMatchObject({
+            error: expect.stringMatching(/bad/gi),
+            message: expect.stringMatching(/fail/gi),
+          });
+        });
+      }
+    );
+
+    describe('when requested with valid id but non existing resource', () => {
+      let id: string;
+      let response: Response;
+
+      beforeEach(async () => {
+        id = 'ffffffffffffffffffffffff';
+        response = await testAgent.get(noteResourcePath.replace(':id', id));
+      });
+
+      test(`should fail with '404 Not Found'`, ({ expect }) => {
+        expect(response.statusCode).toBe(404);
+      });
+
+      test(`should respond with error`, ({ expect }) => {
+        expect(response.body).toMatchObject({
+          error: expect.stringMatching(/not found/gi),
+          message: expect.stringMatching(/fail/gi),
+        });
+      });
+    });
+
+    describe('when requested with valid id', () => {
+      let id: string;
+      let response: Response;
+
+      beforeEach(async () => {
+        id = seededNoteIds[0];
+        response = await testAgent.get(noteResourcePath.replace(':id', id));
+      });
+
+      test(`should respond with '200 OK'`, ({ expect }) => {
+        expect(response.statusCode).toBe(200);
+      });
+
+      test(`should return the data`, ({ expect }) => {
+        expect(response.body).toMatchObject({
+          note: {
+            id,
+            title: expect.any(String),
+            content: expect.any(String),
+          },
         });
       });
     });
