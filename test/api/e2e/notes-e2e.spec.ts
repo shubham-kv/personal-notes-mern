0a413e91609e2a39712d8fafb5ed50b287e7f247
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-04-06 14:30:21 +0530

    test(api): add tests for get notes API

--- test/api/e2e/notes-e2e.spec.ts
+++ test/api/e2e/notes-e2e.spec.ts
@@ -3,22 +3,23 @@ import supertest, { Response } from 'supertest';
 import TestAgent from 'supertest/lib/agent';
 
 import { createApp } from '@server/app';
-import { CreateNoteData } from '@shared/types/api';
+import { CreateNoteData, GetNotesQueryParams } from '@shared/types/api';
 
 import { runDBHooks } from '../setup';
 import { createNoteDataStub } from '../stubs';
-import { invalidCreateNoteData } from '../data';
+import { invalidCreateNoteData, invalidGetNotesQueryParams } from '../data';
 import { notesResourcePath } from '../constants';
 
 describe('Notes API e2e', () => {
   const createNoteRequestLine = `POST ${notesResourcePath}`;
+  const getNotesRequestLine = `GET ${notesResourcePath}`;
   let testAgent: TestAgent;
 
   beforeAll(async () => {
     const app = await createApp();
     testAgent = supertest(app);
   });
-  runDBHooks();
+  runDBHooks(true);
 
   describe(`Create Note API '${createNoteRequestLine}'`, () => {
     describe.each(invalidCreateNoteData)(
@@ -35,8 +36,10 @@ describe('Notes API e2e', () => {
         });
 
         test(`should return errors`, ({ expect }) => {
-          // expect(response.body).toBe(null); // To make the test fail & display the actual from expected vs. actual
-          expect(response.body.errors[0]).toBeDefined();
+          expect(response.body).toMatchObject({
+            error: expect.stringMatching(/bad/gi),
+            message: expect.stringMatching(/fail/gi),
+          })
         });
       }
     );
@@ -55,9 +58,63 @@ describe('Notes API e2e', () => {
       });
 
       test(`should return the created note`, ({ expect }) => {
-        // expect(response.body).toBe(null); // To make the test fail explicitly
         expect(response.body.note).toMatchObject({
           ...data,
+        });
+      });
+    });
+  });
+
+  describe(`Get Notes API '${getNotesRequestLine}'`, () => {
+    describe.each(invalidGetNotesQueryParams)(
+      'when requested with invalid query params',
+      (input) => {
+        let response: Response;
+
+        beforeEach(async () => {
+          response = await testAgent.get(notesResourcePath).query(input);
+        });
+
+        test(`should fail with '400 Bad Request'`, ({ expect }) => {
+          expect(response.statusCode).toBe(400);
+        });
+
+        test(`should respond with error`, ({ expect }) => {
+          expect(response.body).toMatchObject({
+            error: expect.stringMatching(/bad/gi),
+            message: expect.stringMatching(/fail/gi),
+          })
+        });
+      }
+    );
+
+    describe('when requested with valid query params', () => {
+      let query: GetNotesQueryParams;
+      let response: Response;
+
+      beforeEach(async () => {
+        query = {
+          page: 1,
+          pageLimit: 5,
+          search: 'note',
+        };
+        response = await testAgent.get(notesResourcePath).query(query);
+      });
+
+      test(`should respond with '200 OK'`, ({ expect }) => {
+        expect(response.statusCode).toBe(200);
+      });
+
+      test(`should return the data`, ({ expect }) => {
+        expect(response.body).toMatchObject({
+          data: expect.arrayContaining([
+            expect.objectContaining({
+              title: expect.stringMatching(/note/gi),
+            }),
+          ]),
+          total: expect.any(Number),
+          pageLimit: query.pageLimit,
+          page: query.page,
         });
       });
     });
--- test/api/setup.ts
+++ test/api/setup.ts
@@ -1,6 +1,14 @@
 import mongoose from 'mongoose';
 import { afterAll, beforeAll } from 'vitest';
+import { createNote } from '@server/services/notes';
 import { initiateDbConnection, terminateDbConnection } from '@server/lib/db';
+import { createNoteInputs } from './data';
+
+async function seedNotes() {
+  for (let i = 0; i < createNoteInputs.length; i++) {
+    await createNote(createNoteInputs[i]);
+  }
+}
 
 async function clearDatabase() {
   const { connection } = mongoose;
@@ -11,9 +19,13 @@ async function clearDatabase() {
   }
 }
 
-export function runDBHooks() {
+export function runDBHooks(shouldSeed = false) {
   beforeAll(async () => {
     await initiateDbConnection();
+
+    if (shouldSeed) {
+      await seedNotes();
+    }
   });
 
   afterAll(async () => {
