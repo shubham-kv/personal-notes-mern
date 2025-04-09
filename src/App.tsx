b240b2fa0712445193db9dde964a55a673e48e96
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-04-09 11:53:56 +0530

    refactor(ui-app): update home route to /n, update app layout & add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import { Route, Routes } from 'react-router';
+import { Route, Routes, Navigate } from 'react-router';
 import { AppLayout } from '@/components/layouts';
+import { NotFound } from '@/components/NotFound';
 import { ViewNote, ViewNotes } from '@/features/notes';
 
 export default function App() {
   return (
     <Routes>
-      <Route path='/' element={<AppLayout />}>
-        <Route index element={<ViewNotes />} />
-        <Route path='n/:id' element={<ViewNote />} />
+      <Route path='/'>
+        <Route index element={<Navigate to={'/n'} replace={true} />} />
+
+        <Route path='n' element={<AppLayout renderNav={true} />}>
+          <Route index element={<ViewNotes />} />
+          <Route path=':id' element={<ViewNote />} />
+        </Route>
+
+        <Route element={<AppLayout />}>
+          <Route path='404' element={<NotFound />} />
+        </Route>
       </Route>
+
+      <Route path='*' element={<Navigate to={'/404'} replace={true} />} />
     </Routes>
   );
 }
