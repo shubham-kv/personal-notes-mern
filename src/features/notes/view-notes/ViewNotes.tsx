c8215180a4a01aa0b09d9dcc92904fccbbdd1e6f
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-04-11 12:42:36 +0530

    fix(ui-view-notes): runtime errors when some properties are undefined

--- src/features/notes/view-notes/ViewNotes.tsx
+++ src/features/notes/view-notes/ViewNotes.tsx
@@ -46,8 +46,9 @@ export function ViewNotes() {
     );
   }, []);
 
-  const notesWithKey = data?.data.map((n) => ({ key: n.id, ...n })) ?? [];
-  const totalPages = data ? Math.ceil(data.total / queryParams.pageLimit) : 0;
+  const notesWithKey = (data?.data ?? []).map((n) => ({ key: n.id, ...n }));
+  const totalPages =
+    data && data.total ? Math.ceil(data.total / queryParams.pageLimit) : 0;
 
   return (
     <ListProvider
