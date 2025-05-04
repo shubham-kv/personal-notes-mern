c591b7da566ed4f48c22dba19e5bf7c66da53424
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-05-04 21:10:35 +0530

    chore(ui-sign-in): add some content & skeleton loader

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,21 +1,46 @@
 import { useClerk } from '@clerk/clerk-react';
 import { Button } from '@/components/ui/button';
+import { Skeleton } from './ui/skeleton';
+
+function SignInSkeleton() {
+  return (
+    <div>
+      <div className='space-y-2'>
+        <Skeleton className='h-8 w-40 transition-all' />
+        <Skeleton className='h-6 w-sm md:w-lg transition-all' />
+        <Skeleton className='h-6 w-xs md:w-sm transition-all' />
+        <Skeleton className='h-6 w-32 transition-all' />
+      </div>
+      <div></div>
+    </div>
+  );
+}
 
 export function SignIn() {
   const clerk = useClerk();
 
   if (!clerk.loaded) {
-    return <div>Loading...</div>;
+    return <SignInSkeleton />;
   }
 
   return (
-    <div>
-      <Button
-        onClick={() => clerk.openSignIn()}
-        className='hover:cursor-pointer'
-      >
-        Sign In
-      </Button>
+    <div className='flex flex-col gap-4'>
+      <div>
+        <h3 className='my-2'>Sign In</h3>
+        <p className='my-2'>
+          Hello there, please sign in from here to access your well preserved
+          notes.
+        </p>
+      </div>
+
+      <div>
+        <Button
+          onClick={() => clerk.openSignIn()}
+          className='hover:cursor-pointer'
+        >
+          Sign In
+        </Button>
+      </div>
     </div>
   );
 }
